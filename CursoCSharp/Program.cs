6267f780a43e47df46f21519733e1948e2757717
Fabricio Anibal <fabricio_anibal@hotmail.com>
2020-03-31 15:17:15 -0300

    Projeto incial mostrando 'Olá mundo'

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!!");
+            Console.ReadKey();
+        }
+    }
+}
